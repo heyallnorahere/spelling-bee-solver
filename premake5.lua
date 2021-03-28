846678411536073c8dd493c1c06aa4420cae36ac
Cosmodash <theo.github@bedafamily.com>
2021-03-28 13:42:09 -0700

    initial commit

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+workspace "spelling-bee-solver"
+    architecture "x64"
+    targetdir "build"
+    configurations {
+        "Debug",
+        "Release"
+    }
+    flags {
+        "MultiProcessorCompile"
+    }
+    startproject "spelling-bee-solver"
+    filter "system:windows"
+        defines {
+            "_CRT_SECURE_NO_WARNINGS"
+        }
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+project "codegen"
+    location "codegen"
+    kind "ConsoleApp"
+    language "C"
+    staticruntime "on"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    files {
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/src/**.h"
+    }
+    filter "configurations:Debug"
+        symbols "on"
+    filter "configurations:Release"
+        optimize "on"
+project "spelling-bee-solver"
+    location "spelling-bee-solver"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    files {
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/src/**.h",
+    }
+    filter "configurations:Debug"
+        symbols "on"
+    filter "configurations:Release"
+        optimize "on"
+    
