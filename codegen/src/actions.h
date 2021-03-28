fe56247ec8151f3ed1a09e9c6bd92173b06a3ea0
Cosmodash <theo.github@bedafamily.com>
2021-03-28 15:07:55 -0700

    i realized i didnt need a codegen tool!

--- codegen/src/actions.h
+++
@@ -1,4 +0,0 @@
-#ifndef ACTIONS_H
-#define ACTIONS_H
-int action_embed_data(const char* path1, const char* path2);
-#endif
--- premake5.lua
+++ premake5.lua
@@ -14,21 +14,6 @@ workspace "spelling-bee-solver"
             "_CRT_SECURE_NO_WARNINGS"
         }
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-project "codegen"
-    location "codegen"
-    kind "ConsoleApp"
-    language "C"
-    staticruntime "on"
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-    files {
-        "%{prj.name}/src/**.c",
-        "%{prj.name}/src/**.h",
-    }
-    filter "configurations:Debug"
-        symbols "on"
-    filter "configurations:Release"
-        optimize "on"
 project "spelling-bee-solver"
     location "spelling-bee-solver"
     kind "ConsoleApp"
@@ -43,12 +28,6 @@ project "spelling-bee-solver"
     }
     sysincludedirs {
         "vendor/json/include"
-    }
-    links {
-        "codegen"
-    }
-    prebuildcommands {
-        '"%{cfg.targetdir}/../codegen/codegen" embed "../vendor/english-words/words_dictionary.json" "generated_words.h"'
     }
     filter "configurations:Debug"
         symbols "on"
